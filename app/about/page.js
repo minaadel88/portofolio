e775a6bc0e86d5777c6a32d97cd2eca5ef614978
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-09-24 00:29:46 +0300

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -4,23 +4,23 @@ import styles from '../../styles/globals.css'; // Import CSS module for styles
 
 const About = () => {
   return (
-    <div >
+    <div>
       <Header />
       <div className={styles.content}>
         <div className={styles.text}>
-          <p>Hi Iam MINA Adel a junior developer.</p>
-          <p>Front-end React Js</p>
+          <p>Hi, I am Mina Adel, a junior developer.</p>
+          <p>Front-end React.js developer.</p>
         </div>
         <div>
-      
-      <p>Check out my CV:</p>
-      <a href="cv.pdf" target="_blank" rel="noopener noreferrer">
-        Download CV
-      </a>
-    </div>
+          <p>Check out my CV:</p>
+          <a href="cv.pdf" target="_blank" rel="noopener noreferrer">
+            Download CV
+          </a>
+        </div>
       </div>
     </div>
   );
 };
 
 export default About;
+
